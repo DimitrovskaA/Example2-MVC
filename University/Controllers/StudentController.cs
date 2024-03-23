e92bc9bf5f79df0f85008f9fee791469e5b07474
adimitrovska <adimitrovska@tu-sofia.bg>
2024-03-23 13:30:33 +0200

    University

---
+++ University/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.Models;
+namespace University.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<StudentViewModel> students = new List<StudentViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            var studVm = new StudentViewModel();
+            return View(studVm);
+        }
+        public IActionResult CreateStudent(StudentViewModel studentViewModel) 
+        {
+            students.Add(studentViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
